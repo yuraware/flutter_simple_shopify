025a37b02b5466c2923feaaa62a99f8836260961
sam-parks <parkssa123@gmail.com>
2021-07-22 16:26:38 -0400

    token checkout updates

--- lib/graphql_operations/mutations/checkout_complete_token_V2.dart
+++ lib/graphql_operations/mutations/checkout_complete_token_V2.dart
@@ -6,6 +6,133 @@ mutation checkoutCompleteWithTokenizedPaymentV2($checkoutId: ID!, $payment: Toke
   ) {
     checkout {
       id
+      email
+      ready
+      appliedGiftCards {
+        amountUsedV2 {
+          amount
+          currencyCode
+        }
+        balanceV2 {
+          amount
+          currencyCode
+        }
+        id
+        lastCharacters
+        presentmentAmountUsed {
+          amount
+          currencyCode
+        }
+      }
+      requiresShipping
+      shippingLine {
+        handle
+        priceV2 {
+          amount
+          currencyCode
+        }
+        title
+      }
+      shippingAddress {
+        address1
+        address2
+        city
+        company
+        country
+        countryCodeV2
+        firstName
+        formattedArea
+        id
+        lastName
+        latitude
+        longitude
+        name
+        phone
+        province
+        provinceCode
+        zip
+      }
+      completedAt
+      createdAt
+      currencyCode
+       discountApplications(first: 10) {
+        edges{
+          node {
+            allocationMethod
+            targetSelection
+            targetType
+             value {
+              __typename
+              ... on PricingPercentageValue {
+                percentage
+              }
+              ... on MoneyV2 {
+                amount
+                currencyCode
+              }
+            }
+          }
+        }
+      }
+      lineItems(first: 10) {
+        edges {
+          node {
+            id
+            quantity
+            title
+             discountAllocations {
+              allocatedAmount {
+                amount
+                currencyCode
+              }
+            }
+            variant {
+              id
+              priceV2 {
+                amount
+                currencyCode
+              }
+              title
+              image {
+                altText
+                originalSrc
+                id
+              }
+              compareAtPriceV2 {
+                amount
+                currencyCode
+              }
+              weight
+              weightUnit
+              availableForSale
+              sku
+              requiresShipping
+            }
+          }
+        }
+      }
+      note
+      webUrl
+      updatedAt
+      totalTaxV2 {
+        amount
+        currencyCode
+      }
+      totalPriceV2 {
+        amount
+        currencyCode
+      }
+      paymentDueV2 {
+        amount
+        currencyCode
+      }
+      taxesIncluded
+      taxExempt
+      subtotalPriceV2 {
+        amount
+        currencyCode
+      }
+      orderStatusUrl
       order {
         id
         email
