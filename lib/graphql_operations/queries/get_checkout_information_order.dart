d04268ce4482544b086256d8362b370a81a65349
sam-parks <parkssa123@gmail.com>
2021-08-17 08:47:25 -0400

    update order information

--- lib/graphql_operations/queries/get_checkout_information_order.dart
+++ lib/graphql_operations/queries/get_checkout_information_order.dart
@@ -5,23 +5,6 @@ query($id: ID!){
       id
       ready
       email
-      appliedGiftCards {
-        amountUsedV2 {
-          amount
-          currencyCode
-        }
-        balanceV2 {
-          amount
-          currencyCode
-        }
-        id
-        lastCharacters
-        presentmentAmountUsed {
-          amount
-          currencyCode
-        }
-      }
-      requiresShipping
       shippingLine {
         handle
         priceV2 {
@@ -30,95 +13,10 @@ query($id: ID!){
         }
         title
       }
-      availableShippingRates {
-        ready
-        shippingRates {
-          handle
-          title
-          priceV2 {
-            amount
-            currencyCode
-          }
-        }
-      }
-      shippingAddress {
-        address1
-        address2
-        city
-        company
-        country
-        countryCodeV2
-        firstName
-        formattedArea
-        id
-        lastName
-        latitude
-        longitude
-        name
-        phone
-        province
-        provinceCode
-        zip
-      }
+      requiresShipping
       completedAt
       createdAt
       currencyCode
-      discountApplications(first: 10) {
-        edges{
-          node {
-            allocationMethod
-            targetSelection
-            targetType
-             value {
-              __typename
-              ... on PricingPercentageValue {
-                percentage
-              }
-              ... on MoneyV2 {
-                amount
-                currencyCode
-              }
-            }
-          }
-        }
-      }
-      lineItems(first: 10) {
-        edges {
-          node {
-            id
-            quantity
-            title
-            discountAllocations {
-              allocatedAmount {
-                amount
-                currencyCode
-              }
-            }
-            variant {
-              id
-              priceV2 {
-                amount
-                currencyCode
-              }
-              title
-              image {
-                altText
-                originalSrc
-                id
-              }
-              compareAtPriceV2 {
-                amount
-                currencyCode
-              }
-              weight
-              weightUnit
-              availableForSale
-              sku
-              requiresShipping
-            }
-          }
-        }
-      }
       note
       webUrl
       updatedAt
@@ -144,57 +42,11 @@ query($id: ID!){
         amount
         currencyCode
       }
-      orderStatusUrl
       order {
         id
         email
         currencyCode
         customerUrl
-        lineItems(first: 250) {
-          edges {
-            node {
-              currentQuantity
-              discountAllocations {
-                allocatedAmount {
-                  amount
-                  currencyCode
-                }
-              }
-              discountedTotalPrice {
-                amount
-                currencyCode
-              }
-              originalTotalPrice {
-                amount
-                currencyCode
-              }
-              quantity
-              title
-              variant {
-                priceV2 {
-                  amount
-                  currencyCode
-                }
-                title
-                image {
-                  altText
-                  id
-                  originalSrc
-                }
-                compareAtPriceV2 {
-                  amount
-                  currencyCode
-                }
-                weight
-                weightUnit
-                availableForSale
-                sku
-                requiresShipping
-                id
-              }
-            }
-          }
-        }
         name
         orderNumber
         phone
